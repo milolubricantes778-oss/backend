b84e5a5f689293cac08819009cc0f910fb09073d
Juancolque04 <jdiazcolque04@gmail.com>
2025-09-06 13:34:27 -0300

    agregando cors

--- src/config/cors.js
+++ src/config/cors.js
@@ -9,8 +9,8 @@ const corsOptions = {
       "http://localhost:3000",
       "http://localhost:3001",
       "http://localhost:5173",
-      "http://127.0.0.1:3000",
-      "http://127.0.0.1:3001",
+      "https://milolubricantes.site",
+      "https://www.milolubricantes.site",
       "http://127.0.0.1:5173",
     ]
 
